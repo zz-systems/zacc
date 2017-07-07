51b805fd1f962c38e8ed089ab78185eb70c81e14
Sergej Zuyev <sergej.zuyev@zz-systems.net>
2017-07-07 02:56:41 +0200

    CI cfg, x86intrin

--- include/intrin.hpp
+++ include/intrin.hpp
@@ -30,7 +30,7 @@
 /* Microsoft C/C++-compatible compiler */
 #include <intrin.h>
 #include <smmintrin.h>
-#elif defined(__GNUC__) && (defined(__x86_64__) || defined(__i386__))
+#elif (defined(__GNUC__) || defined(__clang__)) && (defined(__x86_64__) || defined(__i386__))
 /* GCC-compatible compiler, targeting x86/x86-64 */
 #include <x86intrin.h>
 #elif defined(__GNUC__) && defined(__ARM_NEON__)
