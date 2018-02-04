15ad87522c8f580e4fb340548184afc566307de7
Sergej Zuyev <sergej.zuyev@zz-systems.net>
2018-02-04 22:16:19 +0100

    Update travis cfg

--- ci/setup-dependencies.sh
+++ ci/setup-dependencies.sh
@@ -12,6 +12,6 @@ if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
     CMAKE_URL="http://www.cmake.org/files/v3.6/cmake-3.6.0-Linux-x86_64.tar.gz"
     mkdir cmake && travis_retry wget --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
     export PATH=${DEPS_DIR}/cmake/bin:${PATH}
-else
-    pip install --user -r ./requirements.txt
 fi
+
+pip install --user -r ./requirements.txt
