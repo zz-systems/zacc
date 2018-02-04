a4b16fa09d9c47348d700bf72d7f5b7d86a8f19d
Sergej Zuyev <sergej.zuyev@zz-systems.net>
2018-02-05 00:27:14 +0100

    Update travis cfg

--- ci/setup-environment.sh
+++ ci/setup-environment.sh
@@ -4,6 +4,8 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     brew update
     brew install python3
     brew install doxygen
+    brew install sphinx-doc
+    brew link sphinx-doc --force
     virtualenv venv -p python3
     source venv/bin/activate
 fi
