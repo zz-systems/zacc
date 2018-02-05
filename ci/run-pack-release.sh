ea374c19b5db80814e9a8d5a7043daefb5d667f1
Sergej Zuyev <sergej.zuyev@zz-systems.net>
2018-02-05 01:39:55 +0100

    Travis cfg, releases

--- ci/run-pack-release.sh
+++ ci/run-pack-release.sh
@@ -2,30 +2,30 @@
 
 cd ${TRAVIS_BUILD_DIR}
 
-mkdir release
+mkdir ${RELEASE_NAME}
 
 # copy binary files
-cp -r ${BUILD_DIR}/bin release/bin
-cp -r ${BUILD_DIR}/lib release/lib
+cp -r ${BUILD_DIR}/bin ${RELEASE_NAME}/bin
+cp -r ${BUILD_DIR}/lib ${RELEASE_NAME}/lib
 
 # copy dependencies
-cp -r ${TRAVIS_BUILD_DIR}/dependencies release/dependencies
+cp -r ${TRAVIS_BUILD_DIR}/dependencies ${RELEASE_NAME}/dependencies
 
 # copy sources
-cp -r ${TRAVIS_BUILD_DIR}/cmake release/cmake
-cp -r ${TRAVIS_BUILD_DIR}/codegen release/codegen
+cp -r ${TRAVIS_BUILD_DIR}/cmake ${RELEASE_NAME}/cmake
+cp -r ${TRAVIS_BUILD_DIR}/codegen ${RELEASE_NAME}/codegen
 
-cp -r ${TRAVIS_BUILD_DIR}/examples release/examples
-cp -r ${TRAVIS_BUILD_DIR}/include release/include
-cp -r ${TRAVIS_BUILD_DIR}/src release/src
-cp -r ${TRAVIS_BUILD_DIR}/test release/test
-cp -r ${TRAVIS_BUILD_DIR}/CMakeLists.txt release/CMakeLists.txt
+cp -r ${TRAVIS_BUILD_DIR}/examples ${RELEASE_NAME}/examples
+cp -r ${TRAVIS_BUILD_DIR}/include ${RELEASE_NAME}/include
+cp -r ${TRAVIS_BUILD_DIR}/src ${RELEASE_NAME}/src
+cp -r ${TRAVIS_BUILD_DIR}/test ${RELEASE_NAME}/test
+cp -r ${TRAVIS_BUILD_DIR}/CMakeLists.txt ${RELEASE_NAME}/CMakeLists.txt
 
-cp ${TRAVIS_BUILD_DIR}/LICENSE release/LICENSE
-cp ${TRAVIS_BUILD_DIR}/README.md release/README.md
+cp ${TRAVIS_BUILD_DIR}/LICENSE ${RELEASE_NAME}/LICENSE
+cp ${TRAVIS_BUILD_DIR}/README.md ${RELEASE_NAME}/README.md
 
 # compress
-zip -r release.zip release
+#zip -r release.zip ${RELEASE_NAME}
 
 
 
