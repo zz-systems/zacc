ef0c7d48bab2bcff93728742f74901b486bfb58a
Sergej Zuyev <sergej.zuyev@zz-systems.net>
2018-02-05 01:59:03 +0100

    Fix errors in travis.ci

--- ci/run-build.sh
+++ ci/run-build.sh
@@ -4,3 +4,4 @@ cd "${TRAVIS_BUILD_DIR}/${BUILD_DIR}"
 
 cmake --build . --config %BUILD_TYPE% --target zacc.system.detect -- -j 4
 cmake --build . --config %BUILD_TYPE% --target zacc.tests.all -- -j 4
+cmake --build . --config %BUILD_TYPE% --target zacc.examples -- -j 4
