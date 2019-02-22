cf87a4603fc68480d44121062951162b60d9e2b0
Sergej Zuyev <sergej.zuyev@zz-systems.net>
2019-02-23 00:20:30 +0100

    Python 3 venv

--- ci/travis/documentation/install.sh
+++ ci/travis/documentation/install.sh
@@ -5,10 +5,11 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew upgrade python3
     brew install doxygen
     brew install sphinx-doc
-    brew link sphinx-doc --force
-
-    source venv/bin/activate
+    brew link sphinx-doc --force    
 fi
 
+pip install --user --upgrade pip virtualenv
 virtualenv -p python$PYTHON venv
+source venv/bin/activate
+
 pip install --user -r ${SCRIPT_STAGE_DIR}/requirements.txt
