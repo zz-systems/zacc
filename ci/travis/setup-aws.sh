2122075ac69c57614095bb2e74a6c46526d60daf
Sergej Zuyev <sergej.zuyev@zz-systems.net>
2019-02-22 19:08:09 +0100

    Move asset generation to build dir

--- ci/travis/setup-aws.sh
+++ ci/travis/setup-aws.sh
@@ -5,5 +5,5 @@ aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
 EOL
 
-mkdir -p ~/package
-aws s3 sync s3://${AWS_BUCKET}/package ~/package
+mkdir -p ${TRAVIS_BUILD_DIR}/package
+aws s3 sync s3://${AWS_BUCKET}/package ${TRAVIS_BUILD_DIR}/package
